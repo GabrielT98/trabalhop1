186bcc2ddfdfb09ec15095a7e6a5168296fc67de
Gabriel Tamiozzo <ra202011267@universidadedevassouras.edu.br>
2022-04-19 20:54:52 -0300

    reestruturando o projeto

--- trabalho01/entity.py
+++ trabalho01/entity.py
@@ -1,23 +1,76 @@
 from typing import List
+class Video():
+    def __int__(self):
+        self.__id = None
+        self.__url_video = None
+        self.__url_foto = None
+        self.__titulo = None
+        self.__descricao = None
+        self.__qtd_curtida = None
+        self.__qtd_visualizacao = None
+        self.__data_pub = None
 
-
-class Video:
-    def __int__(self,id:int,url_video:str,url_foto:str,titulo:str,descricao:str,qtd_curtida:int,qtd_visualizacao:int,data_pub:str):
+    def set_id(self,id):
         self.__id = id
-        self.__url_video = url_video
-        self.__url_foto = url_foto
-        self.__titulo = titulo
-        self.__descricao = descricao
-        self.__qtd_curtida = qtd_curtida
-        self.__qtd_visualizacao = qtd_visualizacao
-        self.__data_pub = data_pub
-
-class Categoria:
-    def __int__(self,id:int,titulo:str,descricao:str,foto:str,videos:List):
-        self.__id = id
-        self.__titulo = titulo
-        self.__descricao = descricao
+    def set_url_video(self,video):
+        self.__url_video = video
+    def set_url_foto(self,foto):
         self.__url_foto = foto
+    def set_titulo(self,titulo):
+        self.__titulo = titulo
+    def set_descricao(self,descricao):
+        self.__descricao = descricao
+    def set_curtidas(self,curtidas):
+        self.__qtd_curtida = curtidas
+    def set_visualizacao(self,visu):
+        self.__qtd_visualizacao = visu
+    def set_data_pub(self,data):
+        self.__data_pub = data
+
+    def get_id(self):
+        return self.__id
+    def get_url_video(self):
+        return self.__url_video
+    def get_url_foto(self):
+        return self.__url_foto
+    def get_descricao(self):
+        return self.__descricao
+    def get_titulo(self):
+        return self.__titulo
+    def get_qtd_curtida(self):
+        return self.__qtd_curtida
+    def get_qtd_visualizacao(self):
+        return self.__qtd_visualizacao
+    def get_data_pub(self):
+        return self.__data_pub
+
+class Categoria():
+    def __int__(self):
+        self.__id = None
+        self.__titulo = None
+        self.__descricao = None
+        self.__url_foto = None
+        self.__videos = None
+
+    def set_id(self,id):
+        self.__id = id
+    def set_titulo(self,titulo):
+        self.__titulo = titulo
+    def set_descricao(self,descricao):
+        self.__descricao = descricao
+    def set_url_foto(self,foto):
+        self.__url_foto= foto
+    def set_videos(self,videos:List):
         self.__videos = videos
 
+    def get_id(self):
+        return self.__id
+    def get_titulo(self):
+        return self.__titulo
+    def get_descricao(self):
+        return self.__descricao
+    def get_foto(self):
+        return self.__url_foto
+    def get_videos(self):
+        return self.__videos
 
